9300c6df513c3d96e429d20f587b0b752769b107
elkanajnrmanullang <elkanamanullang7@gmail.com>
2026-02-01 19:43:07 +0700

    update

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,10 +1,29 @@
-<?php 
+<?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProductController;
-use App\Http\Controllers\Api\ForgotPasswordController;
-use App\Http\Controllers\Api\UserController;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\File;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cors-assets/{path}', function ($path) {
+    $fullPath = storage_path('app/public/' . $path);
+
+    if (!File::exists($fullPath)) {
+        abort(404);
+    }
+
+    $file = File::get($fullPath);
+    $type = File::mimeType($fullPath);
+
+    return Response::make($file, 200, [
+        'Content-Type' => $type,
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Allow-Methods' => 'GET, OPTIONS',
+        'Access-Control-Allow-Headers' => '*',
+        'Cross-Origin-Resource-Policy' => 'cross-origin',
+        'Cache-Control' => 'public, max-age=86400',
+    ]);
+})->where('path', '.*');
